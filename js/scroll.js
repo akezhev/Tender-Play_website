0c13c4c1ee06a1263a171fd37dc6f9464a7a5557
akezhev <akezh@mail.ru>
2025-06-01 15:23:04 +0300

    modified some file

--- js/scroll.js
+++ js/scroll.js
@@ -15,16 +15,11 @@ for (let anchor of anchors) {
 }
 
 // scroll_animation_block------------------------
-function onEntry(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-     change.target.classList.add('element-show');
-    }
-  });
-}
-
 let options = {
-  threshold: [0.5] };
+  rootMargin: '0px',
+  threshold: [0.2, 0.5, 0.75]
+};
+
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.element-animation');
 
@@ -33,35 +28,53 @@ for (let elm of elements) {
 }
 
 // scroll_animation_block_4----------------------------
-function onEntry2(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-     change.target.classList.add('element-show_2');
-    }
-  });
-}
 let options2 = {
-  threshold: [0.5] };
+  rootMargin: '0px',
+  threshold: [0.2, 0.5, 0.75]
+};
+
 let observer2 = new IntersectionObserver(onEntry2, options2);
 let elements2 = document.querySelectorAll('.element-animation_2');
-for (let elm2 of elements2) {
-  observer2.observe(elm2);
+
+for (let elm of elements2) {
+  observer2.observe(elm);
 }
 
 // ----------------------------------------------------
-function onEntry3(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-     change.target.classList.add('element-show_3');
+let options3 = {
+  rootMargin: '0px',
+  threshold: [0.2, 0.5, 0.75]
+};
+
+let observer3 = new IntersectionObserver(onEntry3, options3);
+let elements3 = document.querySelectorAll('.element-animation_3');
+
+for (let elm of elements3) {
+  observer3.observe(elm);
+}
+
+function onEntry(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('element-show');
     }
   });
 }
-let options3 = {
-  threshold: [0.5] };
-let observer3 = new IntersectionObserver(onEntry3, options2);
-let elements3 = document.querySelectorAll('.element-animation_3');
-for (let elm3 of elements3) {
-  observer3.observe(elm3);
+
+function onEntry2(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('element-show_2');
+    }
+  });
+}
+
+function onEntry3(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('element-show_3');
+    }
+  });
 }
 
 
